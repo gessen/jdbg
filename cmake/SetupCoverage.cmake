e6613f04d0ed83578929b08f59fe3f8b605c7694
Jacek Swierk <jacek.swierk@gmail.com>
2025-08-29 13:42:36 +0200

    Update code coverage generation

--- cmake/SetupCoverage.cmake
+++ cmake/SetupCoverage.cmake
@@ -1,7 +1,7 @@
 if(JDBG_ENABLE_COVERAGE)
   if(NOT CMAKE_BUILD_TYPE STREQUAL "Debug")
     message(WARNING
-      "Code coverage results with an optimised build may be misleading"
+      "Code coverage results with an optimized build may be misleading"
     )
   endif()
 endif()
@@ -23,18 +23,12 @@ function(_setup_coverage_html target exec args output_dir)
 endfunction()
 
 function(_setup_coverage_text target exec args output_dir)
-  set(output_file "${output_dir}/coverage.txt")
-  list(APPEND args --output="${output_file}")
   list(APPEND args --txt)
 
   add_custom_target(${target}-text
     COMMAND ${exec} ${args}
     WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
     COMMENT "Running gcovr to produce a text report"
-  )
-  add_custom_command(TARGET ${target}-html POST_BUILD
-    COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow
-    "Open file://${output_file} to see a text coverage report"
   )
 endfunction()
 
@@ -44,9 +38,7 @@ function(setup_coverage)
     if(LlvmCov_FOUND)
       set(cov_executable "${LLVM_COV_EXECUTABLE} gcov")
     endif()
-  endif()
-
-  if(NOT cov_executable)
+  else()
     find_package(GCov)
     if(GCov_FOUND)
       set(cov_executable "${GCOV_EXECUTABLE}")
@@ -59,7 +51,7 @@ function(setup_coverage)
     set(coverage_target coverage)
   endif()
 
-  find_package(GCovr 4.2)
+  find_package(GCovr 7.1)
 
   if(NOT cov_executable OR NOT GCovr_FOUND)
     message(WARNING "${coverage_target} target will be unavailable")
@@ -68,7 +60,7 @@ function(setup_coverage)
 
   set(output_dir "${PROJECT_BINARY_DIR}/coverage")
   file(MAKE_DIRECTORY "${output_dir}")
-  
+
   set(gcovr_args)
   list(APPEND gcovr_args --gcov-executable=${cov_executable})
   list(APPEND gcovr_args --root=${PROJECT_SOURCE_DIR})
@@ -79,10 +71,16 @@ function(setup_coverage)
   endif()
   list(APPEND gcovr_args ${PROJECT_BINARY_DIR})
 
-  _setup_coverage_html("${coverage_target}" "${GCOVR_EXECUTABLE}" "${gcovr_args}" "${output_dir}")
-  _setup_coverage_text("${coverage_target}" "${GCOVR_EXECUTABLE}" "${gcovr_args}" "${output_dir}")
-
-  add_custom_target(${coverage_target}
-    DEPENDS ${coverage_target}-html ${coverage_target}-text
+  _setup_coverage_html("${coverage_target}"
+    "${GCOVR_EXECUTABLE}"
+    "${gcovr_args}"
+    "${output_dir}"
   )
+  _setup_coverage_text("${coverage_target}"
+    "${GCOVR_EXECUTABLE}"
+    "${gcovr_args}"
+    "${output_dir}"
+  )
+
+  add_custom_target(${coverage_target} DEPENDS ${coverage_target}-text)
 endfunction()
